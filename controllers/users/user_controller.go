1884dbfc1efd1e5d952abe6a483b1db8f595f035
Manuel Hernandez <manuehdez@MBA-Manuel.local>
2020-03-09 19:53:38 -0700

    Starting from our domain 2

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,14 +1,12 @@
 package users
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/manuelhdez/bs_users-api/domain/users"
 	"github.com/manuelhdez/bs_users-api/services"
+	"github.com/manuelhdez/bs_users-api/utils/errors"
 )
 
 func GetUser(c *gin.Context) {
@@ -21,16 +19,14 @@ func SearchUser(c *gin.Context) {
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		return
-	}
-	if err := json.Unmarshal(bytes, &user); err != nil {
-		fmt.Println(err.Error())
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json request")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
 		return
 	}
 	c.JSON(http.StatusCreated, result)
